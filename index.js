10b52e0a9da0d9f5e219925b434d3a4c87a877ad
PAVAN SAI TEJA <53715933+pavan-up@users.noreply.github.com>
2023-05-02 12:01:06 +0530

    init

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const cors = require('cors');
+const Jimp = require('jimp');
+const { Readable } = require('stream');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const config = {
+  location: 'San Francisco',
+  change: '$5.50',
+  time: '3:30AM',
+};
+
+app.get('/get_text_image', async (req, res) => {
+  const text = `Location: ${config.location}\nChange: ${config.change}\nTime: ${config.time}`;
+
+  const img = await new Jimp(1000, 200, 0xFFFFFFFF);
+  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+
+  img.print(font, 10, 40, text);
+
+  const buffer = await img.getBufferAsync(Jimp.MIME_JPEG);
+  const stream = Readable.from(buffer);
+
+  res.setHeader('Content-Type', 'image/jpeg');
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+
+  stream.pipe(res);
+});
+
+app.post('/set_variables', (req, res) => {
+  config.location = req.body.location;
+  config.change = req.body.change;
+  config.time = req.body.time;
+
+  res.send('String variables updated successfully.');
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
